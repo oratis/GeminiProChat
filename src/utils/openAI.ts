e7897a79bfdace0fdcca79583eb26e2e1b81edb9
oratis <wangharp@gmail.com>
2024-04-26 19:06:25 +0800

    Update openAI.ts

--- src/utils/openAI.ts
+++ src/utils/openAI.ts
@@ -7,9 +7,8 @@ const genAI = apiBaseUrl
   ? new GoogleGenerativeAI(apiKey, apiBaseUrl)
   : new GoogleGenerativeAI(apiKey)
 
-export const startChatAndSendMessageStream = async(history: ChatMessage[], newMessage: sysprompt+string) => {
+export const startChatAndSendMessageStream = async(history: ChatMessage[], newMessage: string) => {
   const model = genAI.getGenerativeModel({ model: 'gemini-pro' })
-  const sysprompt="你是民用机场环境监控系统保护区数据查询AI，你不是Gemini，工程师会输入一个点的坐标，你需要说明这个坐标具体的位置和规定。";
   const chat = model.startChat({
     history: history.map(msg => ({
       role: msg.role,
